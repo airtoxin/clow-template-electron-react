d307d74ad60995471bab82e976d5aab986da5da2
airtoxin <airtoxin@icloud.com>
2016-09-22 04:24:22 +0900

    Initialized by clow.

---
+++ clow.js
@@ -0,0 +1,57 @@
+module.exports = [
+  {
+    type: "template",
+    src: {
+      cwd: "templates",
+      pattern: "**/*"
+    },
+    dest: ".",
+    args: {
+      name: null,
+      description: null,
+      year: (new Date()).getFullYear()
+    }
+  },
+  {
+    type: "npm-install",
+    dependencies: [
+      "babel-runtime",
+      "baobab",
+      "baobab-react",
+      "react",
+      "react-dom"
+    ],
+    devDependencies: [
+      "babel-cli",
+      "babel-eslint",
+      "babel-plugin-transform-runtime",
+      "babel-preset-es2015",
+      "babel-preset-es2016",
+      "babel-preset-es2017",
+      "babel-preset-react",
+      "babel-preset-stage-2",
+      "babel-preset-stage-3",
+      "babel-register",
+      "devtron",
+      "electron",
+      "electron-packager",
+      "eslint",
+      "eslint-config-airbnb",
+      "eslint-plugin-async-await",
+      "eslint-plugin-babel",
+      "eslint-plugin-import",
+      "eslint-plugin-jsx-a11y",
+      "eslint-plugin-react",
+      "mocha",
+      "rimraf"
+    ]
+  },
+  {
+    type: "shell",
+    commands: [
+      "git init",
+      "git add .",
+      "git commit -m 'Initialized by clow-template-electron-react.'"
+    ]
+  }
+];
